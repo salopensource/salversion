7cc25c0cdaec4edbc9f760d23d18f52d70b9f90c
Graham Gilbert <graham.gilbert@airbnb.com>
2018-03-01 10:03:24 +0000

    Static dir

--- salversion/settings.py
+++ salversion/settings.py
@@ -27,6 +27,27 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+APPEND_SLASH = True
+PROJECT_DIR = os.path.abspath(
+    os.path.join(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        ), os.path.pardir))
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+# URL prefix for static files.
+# Example: "http://example.com/static/", "http://static.example.com/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, 'site_static'),
+)
+
 
 # Application definition
 
