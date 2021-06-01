b2b04902d94bfae3eda384ae7eacf87fc2a96c01
Graham Gilbert <graham.gilbert@airbnb.com>
2021-05-31 20:32:54 -0700

    string

--- cmd/salversion/main.go
+++ cmd/salversion/main.go
@@ -18,6 +18,7 @@ func init() {
 }
 
 func main() {
+	ctx := context.Background()
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		salversion.PostHandler(w, r, Version)
@@ -32,20 +33,20 @@ func main() {
 		port = "8080"
 		log.Infof("Defaulting to port %s", port)
 	}
-	go getVersion()
+	go getVersion(ctx)
 	log.Printf("Listening on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Error(err)
 	}
 }
 
-func getVersion() {
-	ctx := context.Background()
+func getVersion(ctx context.Context) {
 	for {
-		Version, err := salversion.GetSalVersion(ctx)
+		salVersion, err := salversion.GetSalVersion(ctx)
 		if err != nil {
 			log.Error(err)
 		}
+		Version = salVersion.CurrentVersion
 		log.Info(Version)
 		time.Sleep(1 * time.Hour)
 	}
